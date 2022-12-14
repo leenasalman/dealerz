80c4100688f364d8fced18c85ad8e9005c936d41
leenasalman <leenasalman.11@gmail.com>
2022-12-14 22:22:24 +0400

    modified components

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductList from "../Products/ProductList";
+
+function Home() {
+  return (
+    <>
+      <ProductList />
+    </>
+  );
+}
+
+export default Home;
